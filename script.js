d511f089774bae8a070e20d782a5b2bdb3708b81
raymondtechie <37636936+raymondtechie@users.noreply.github.com>
2018-06-21 16:30:24 -0400

    Javascript practice

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 // var userName = prompt("what is your name?");
 // alert("Nice to meet you, "+ userName);
 // console.log("Also great to meet you, "+userName);z
+
+var h1 = document.querySelector("h1");
+var isRed = false;
+
+setInterval(function(){
+    if (isRed){
+        h1.style.background ="white";
+    } else {
+        h1.style.background ="red";
+    }
+    isRed= !isRed;
+}, 100);
+//h1.style.color = "blue";
